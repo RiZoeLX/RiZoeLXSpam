d545fac70bab4d67923fbeaea9a88f6a8f09331b
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-05-02 21:36:46 +0530

    Delete assistant directory

--- assistant/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import asyncio
-import telethon.utils
-from telethon import TelegramClient
-from RiZoeLXSpam import BOT_TOKEN, API_ID, API_HASH
-
-RiZoeL = TelegramClient('RiZoeL', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
