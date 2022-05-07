b6dcc6a71b15983f41158c02469fe8b98aeca7a0
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-05-07 12:28:21 +0530

    Update scrape.py

--- RiZoeLXSpam/assistant/plugins/scrape.py
+++ RiZoeLXSpam/assistant/plugins/scrape.py
@@ -13,8 +13,7 @@ ABORT = "**All process Cancelled !**"
 
 @RiZoeL.on(events.NewMessage(pattern="[!/]scrape"))
 async def scrape(event):
-     if event.sender_id not in DEV:
-           return
+  if event.sender_id not in DEV:
      if event.is_group:
           return await event.reply("**Use this Cmd in PM !!**")
      async with RiZoeL.conversation(event.chat_id) as rizx:
